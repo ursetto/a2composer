e40b17db967225f6318cdd06e2d934ffe7dd7f57
Jim Ursetto <jim@3e8.org>
2020-01-21 18:04:32 -0600

    parse 4x section, EQU

--- midi-magic/tfbd.py
+++ midi-magic/tfbd.py
@@ -24,6 +24,7 @@ def decode_2x(f):
     print(f"# 2x section ({section_count} records)")
     for i in range(section_count):
         (rtype, var_len, offset, area_len) = read_unpack(f, "<BBIH")
+        assert (rtype & 0xf0) == 0x20
         if rtype == 0x29:
             assert var_len == 0
             print(f"ASC ORG+${offset:04x}, ${area_len:02x}")
@@ -31,13 +32,27 @@ def decode_2x(f):
             assert var_len == 0
             print(f"DA  ORG+${offset:04x}, ${area_len:02x}")
         else:
-            assert (rtype & 0xf0) == 0x20
             print(f"{rtype:02x} {var_len:02x} {offset:08x} {area_len:04x}")
 
 
-
 def decode_4x(f):
-    pass
+    # TODO: Properly compute ORG offset
+    (section_count, ) = read_unpack(f, "<H")
+    print(f"# 4x section ({section_count} records)")
+    
+    for i in range(section_count):
+        (rtype, var_len, address, count) = read_unpack(f, "<BBIH")
+        var_data = b''
+        assert (rtype & 0xf0) == 0x40
+        if var_len != 0:
+            assert var_len == read_unpack(f, "<B")[0]
+            (var_data, ) = struct.unpack(f'{var_len}s', f.read(var_len))
+            var_data = bytes(map(lambda x: x & 0x7f, var_data))
+            var_data = var_data.decode('ascii')
+        if rtype == 0x44:
+            print(f"EQU ${address:04x}, {var_data}")
+        else:
+            print(f"{rtype:02x} {var_len:02x} {address:08x} {count:04x} {var_data}")
 
 
 def decode_6x(f):

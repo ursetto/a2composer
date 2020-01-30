4762d810823d7f26d262b2e3611b27796a8af9ae
Jim Ursetto <jim@3e8.org>
2020-01-30 17:06:38 -0600

    mcat: read all catalog files

--- midi-magic/mcat.s
+++ midi-magic/mcat.s
@@ -1,3 +1,9 @@
+* CATALOG routines - Beneath Apple DOS, PDF page 109
+* File descriptive entry format - Beneath Apple DOS, PDF page 41
+
+* Issues: BRUN crashes (tries to read empty sector 11 0D);
+*         13FCG gives graphical corruption in monitor after
+*         second time (call 5116 works fine from BASIC)
 
 BUFPTR      equ $9600         ; buffer 3, typically unused
 IOB         equ $B7E8
@@ -7,47 +13,74 @@ IOB_SECTOR  equ IOB + 5
 IOB_BUFR    equ IOB + 8
 IOB_CMD     equ IOB + $C
 IOB_ERR     equ IOB + $D
-VTOC        equ BUFPTR
-VTOC_TRACK  equ VTOC + 1
-VTOC_SECTOR equ VTOC + 2
+CAT         equ BUFPTR
+CAT_TRACK   equ CAT + 1
+CAT_SECTOR  equ CAT + 2
+CAT_FDIDX   equ $0B           ; first file descriptive entry in catalog sector
+FD_TRACK    equ BUFPTR + 0    ; immediate index into FILEDESC
+FD_NAME     equ BUFPTR + 3    ; immediate index into FILEDESC
 RWTS        equ $03D9         ; or direct to $B7B5
 PRBYTE      equ $FDDA
+CROUT       equ $FD8E
 COUT        equ $FDED
 
 * Warning: Although Wagner's Assembly Lines says file buffer 3 is
 * typically unused, IOB_BUFR is actually $9600 at DOS boot time, 
 * implying it is in use.
-TRK         equ $05
-SECT        equ $06
+TRK         equ $05           ; track to read in READSECT
+SECT        equ $06           ; sector to read in READSECT
+FDIDX       equ $07           ; index to current file descriptive entry
 
         org $13FC
 
 * Read VTOC sector.
+START
         lda #$11
         sta TRK
         lda #$00
         sta SECT
         jsr READSECT
-        bcc :cont
+        bcc READCAT
         brk
-:cont
-        lda VTOC_TRACK
-        ldy VTOC_SECTOR
-* read catalog sector A,Y and 
-:cat    sta TRK            ; don't really need this indirection
+READCAT
+        lda CAT_TRACK      ; track/sector at same offset in VTOC and CAT
+        beq :end
+        ldy CAT_SECTOR
+        sta TRK            ; don't really need this indirection
         sty SECT           ; but it's useful for debugging
         jsr READSECT
-        bcc :read
+        bcc :readfiles
         brk
+
+* Note: DOS CATALOG exits if it encounters a filename descriptor
+* entry with track == 0.  The catalog is initialized to
+* 15 empty blocks all linked together, and deleted files are
+* flagged with $FF (not zeroed), so traversing the whole
+* catalog is wasteful.
+
+:readfiles
+        ldx #CAT_FDIDX
+        stx FDIDX
 :readname
-* Here we should process all filenames in this catalog sector,
-* then fall through to this code.
-
-        ldy VTOC_SECTOR
-        lda VTOC_TRACK
-        bne :cat           ; fixme -- unknown end condition
-        rts
-
+        lda FD_TRACK,x
+        beq :end                ; available entry -- end of catalog
+        bmi :nxtfile            ; deleted file, ignore
+        ldy #30
+:nxtchr lda FD_NAME,x           ; indexed from start of struct
+        jsr COUT
+        inx
+        dey
+        bne :nxtchr
+        jsr CROUT
+:nxtfile 
+        clc
+        lda FDIDX
+        adc #$23                ; next file descriptive entry
+        beq READCAT             ; end of sector
+        tax
+        stx FDIDX
+        jmp :readname
+:end    rts
 
 * Use standard DOS IOB which is already set up to
 * refer to the current slot and drive. We only need
@@ -84,7 +117,5 @@ PRTRKSEC
         jsr COUT
         lda SECT
         jsr PRBYTE
-        lda #$8D
-        jsr COUT
+        jsr CROUT
         rts
-        

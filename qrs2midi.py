8b1133c20e164c76fe535fbaed9983824a2cfc72
Jim Ursetto <jim@3e8.org>
2020-02-04 23:26:55 -0600

    qrs2midi: implement reasonable tempo, pull from filename

--- qrs2midi.py
+++ qrs2midi.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 from pprint import pprint as pp
 import argparse
+import sys
 
 
 def decode_file_to_bytes(f):
@@ -47,7 +48,7 @@ def decode_qrs_to_ir(f, offset):
     return ir
 
 
-def encode_ir_to_midi(ir, filename):
+def encode_ir_to_midi(ir, filename, tempo=94):
     import midi
     # Instantiate a MIDI Pattern (contains a list of tracks)
     pattern = midi.Pattern()
@@ -58,11 +59,20 @@ def encode_ir_to_midi(ir, filename):
 
     tick = 0
     velocity = 70
+    # 5.55 divisor is pretty accurate for CHORUS LINE MEDLEY and RHAPSODY IN BLUE.
+    # Speed may vary during song -- perhaps Apple is slower when more
+    # notes are played? CALL ME IRRESPONSIBLE is not quite as accurate,
+    # nor is DO YOU THINK I'M SEXY (which is also corrupt).
+    delay = tempo / 5.55              # 5.5 too slow, 5.75 too fast, 5.6 *slightly* too fast
     
     for inst in ir:
         op, val = inst
         if op == 'wait':
-            tick = val * 15   # about 15 for my stuff, about 25 for theirs
+            # Default tempo (^) = 94
+            # Other tempo (H) = 72
+            # Tempo is actually just delay time, so is probably a straight division
+            # Perhaps multiplier = tempo / 4 is a good start
+            tick = int(round(val * delay))   # delay is about 15 for my stuff, about 25 for theirs
         elif op == 'on':
             track.append(midi.NoteOnEvent(tick=tick, velocity=velocity, pitch=val))
             tick = 0
@@ -80,13 +90,20 @@ def main():
     ap = argparse.ArgumentParser()
     ap.add_argument('filename')
     ap.add_argument('--offset', type=int, default=0x40)
+    ap.add_argument('--tempo', type=int)    # no default here
     args = ap.parse_args()
 
+    tempo = 94
+    if args.tempo:
+        tempo = args.tempo
+    elif args.filename[0] == '^':
+        tempo = ord(args.filename[1])
+
     with open(args.filename, "rb") as f:
+        print("Transcoding to " + args.filename + ".mid, tempo " + str(tempo), file=sys.stderr)
         ir = decode_qrs_to_ir(f, args.offset)
         # pp(ir)
-        encode_ir_to_midi(ir, args.filename + '.mid')
-
+        encode_ir_to_midi(ir, args.filename + '.mid', tempo)
 
 if __name__ == '__main__':
     main()
